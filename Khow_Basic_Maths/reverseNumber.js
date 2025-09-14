5da7b4a3b533174e6f1e594139f630f4d26facc9
Ashish Gaikwad <work.ashish.gaikwad@gmail.com>
2025-09-14 12:38:38 +0530

    solved using batter approch

--- Khow_Basic_Maths/reverseNumber.js
+++ Khow_Basic_Maths/reverseNumber.js
@@ -1,15 +1,13 @@
-
-
 function reverseNumber(num) {
-    // 1. convert negative number to positive 
+    // 1. convert negative number to positive
     num = Math.abs(num);
 
-    // 2. convert number to string 
+    // 2. convert number to string
     const numToStr = num.toString();
 
     let ans = "";
 
-    // 3. loop in each character in reverse order and store concatenated result 
+    // 3. loop in each character in reverse order and store concatenated result
 
     for (let i = numToStr.length - 1; i >= 0; i--) {
         ans += numToStr[i];
@@ -18,8 +16,24 @@ function reverseNumber(num) {
     return Number(ans);
 }
 
+function reverseString2(num) {
+    return num.toString().split("").reverse().join("");
+}
 
-const num = 1234;
-const ans = reverseNumber(num);
+
+function reverseString3(num) {
+    let reverse = 0;
+
+    while (num !== 0) {
+        let lastDigit = num % 10;
+        reverse = (reverse * 10) + lastDigit;
+        num = Math.floor(num / 10);
+    }
+
+    return reverse;
+}
+
+const num = 0;
+const ans = reverseString3(num);
 
 console.log("Reversed Number:-", ans);
